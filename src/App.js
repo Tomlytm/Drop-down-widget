3576920eefc33f12b68b36d207333c0cf2112c20
jesse <tomilayojesse@gmail.com>
2022-09-28 19:32:11 +0100

    making some commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropdown from "./components/Dropdown";
+
+const options = [
+  {
+   label: 'The color blue',
+   value: 'blue' 
+  }, 
+  {
+   label: 'The color red',
+   value: 'red' 
+  }, 
+  {
+   label: 'The color green',
+   value: 'green' 
+  }, 
+  
+]
 function App() {
+  
+const [selected, setSelected] =useState(options[0])
   return (
-    <Dropdown />
+    <Dropdown  
+      selected = {selected}
+      onInputClick= {setSelected} 
+      options= {options} />
   );
 }
 
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,10 +1,32 @@
 import React from "react";
 
-const Dropdown =()=>{
+const Dropdown =( { options, selected, onInputClick })=>{
+ const renderedItems = options.map((item)=>{
 
+    if(selected.label === item.label){
+        return null
+    }
     return(
-        <div>
-            Dropdown
+        <div  
+            onClick={()=>{onInputClick(item)}}
+            key={item.value} 
+            className="item"
+            >
+            {item.label}
+        </div>
+    )
+ })
+    return(
+        <div className="ui form">
+            <div className="field">
+                <label className="label">Select a Color</label>
+                <div className="ui selection dropdown visible active">
+                    <i className="dropdown icon"></i>
+                    <div className="text">{selected.label}</div>
+                    <div className="menu visible transition">{renderedItems}</div>
+                </div>
+
+            </div>
         </div>
     )
 }
